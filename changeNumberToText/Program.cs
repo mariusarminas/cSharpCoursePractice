fc12d73365b07e348b7814c5ef96e20cdede8a67
mariusarminas <marius9@inbox.lt>
2018-11-18 18:00:20 +0200

    -19 to 19

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -92,37 +92,36 @@ class Program
 
 
         //Sukurti funkciją kuri patikrina ar tekstas yra skaičius. Grąžina true jei teisingas.
-       
-            int SkaiciusMin = -9;
-            int SkaiciusMax = 9;
-            int n;
-            Console.WriteLine("Iveskite skaiciu");
-            string Skaicius = Console.ReadLine();
-            var IsNumber = int.TryParse(Skaicius, out n);
-        if (IsNumber == true)
+
+
         {
-            Atsakymas(n, SkaiciusMin, SkaiciusMax);
-        }
-        else
-        {
-            Console.WriteLine("Ivestas simbolis nera skaicius");
-        }
+            int Skaicius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Isvedimas(Skaicius));
             Console.ReadKey();
         }
-        static void Atsakymas(int n, int SkaiciusMin, int SkaiciusMax)
-        {
-            if (n > SkaiciusMin && n < SkaiciusMax)
-            {
-                Console.WriteLine("Skaicius patenka i " + SkaiciusMin + " ir " + SkaiciusMax + " rezius");
-            }
-            else
-            {
-                Console.WriteLine("Skaicius nepatenka i " + SkaiciusMin + " ir " + SkaiciusMax + " rezius");
-            }
 
+        string Isvedimas(int skaicius)
+        {
+            string[] Pavadinimai = new string[]
+            {
+            "nulis", "vienas", "du", "trys", "keturi", "penki", "sesi", "septyni", "astuoni", "devyni",
+            "desimt", "vienualika", "dvylika", "trylika", "keturiolika", "penkiolika", "sesiolika", "septyniolika", "astuoniolika", "devyniolika"
+            };
+
+            int indeksas = skaicius;
+            if (indeksas < 0)
+                indeksas = indeksas * -1;
+
+            string tekstas = Pavadinimai[indeksas];
+            if (skaicius < 0)
+                tekstas = "minus " + tekstas;
+
+            return tekstas;
         }
 
-        //Sukurti funkciją kuri konvertuoti string į int, patikrinti ar skaičius teisinguose rėžiuose: -9 ... 9.
+
+
 
 
     }
+}
