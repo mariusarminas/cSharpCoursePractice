f1346f0351d8b0e951445fbe3920493437e0f013
mariusarminas <marius9@inbox.lt>
2018-11-19 20:45:56 +0200

    Comment

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -5,8 +5,6 @@ class Program
 
     static void Main()
     {
-        //Sukurti funkciją kuri patikrina ar tekstas yra skaičius. Grąžina true jei teisingas.
-
         int SkaiciusMin = -9;
         int SkaiciusMax = 9;
         int n;
@@ -14,25 +12,7 @@ class Program
         string Skaicius = Console.ReadLine();
         var IsNumber = int.TryParse(Skaicius, out n);
         Console.WriteLine(IsNumber);
-        Console.ReadKey();
 
-    }
-
-}
-
-
-//Sukurti funkciją kuri konvertuoti string į int, patikrinti ar skaičius teisinguose rėžiuose: -9 ... 9.
-class Program
-{
-
-    static void Main()
-    {
-int SkaiciusMin = -9;
-int SkaiciusMax = 9;
-int n;
-Console.WriteLine("Iveskite skaiciu");
-            string Skaicius = Console.ReadLine();
-var IsNumber = int.TryParse(Skaicius, out n);
         if (IsNumber == true)
         {
             Atsakymas(n, SkaiciusMin, SkaiciusMax);
@@ -41,8 +21,8 @@ var IsNumber = int.TryParse(Skaicius, out n);
         {
             Console.WriteLine("Ivestas simbolis nera skaicius");
         }
-            Console.ReadKey();
-        }
+         Console.ReadKey();
+    }
     static void Atsakymas(int n, int SkaiciusMin, int SkaiciusMax)
     {
         if (n > SkaiciusMin && n < SkaiciusMax)
@@ -55,42 +35,37 @@ var IsNumber = int.TryParse(Skaicius, out n);
         }
     }
 
+}
+      /*    static void Zodziai()
+        {
 
 
-
-    //sukurti funkciją, kuri gali atspausdinti skaičius nuo -19 … 19
-    class Program
-{
-
-    static void Main()
-    {
+            Console.WriteLine("Iveskite skaiciu");
             int Skaicius = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(Isvedimas(Skaicius));
             Console.ReadKey();
-        }
 
-        string Isvedimas(int skaicius)
-        {
-            string[] Pavadinimai = new string[]
+    
+            string Isvedimas(int skaicius)
             {
+                string[] Pavadinimai = new string[]
+                {
             "nulis", "vienas", "du", "trys", "keturi", "penki", "sesi", "septyni", "astuoni", "devyni",
             "desimt", "vienualika", "dvylika", "trylika", "keturiolika", "penkiolika", "sesiolika", "septyniolika", "astuoniolika", "devyniolika"
-            };
+                };
 
-            int indeksas = skaicius;
-            if (indeksas < 0)
-                indeksas = indeksas * -1;
+                int indeksas = skaicius;
+                if (indeksas < 0)
+                    indeksas = indeksas * -1;
 
-            string tekstas = Pavadinimai[indeksas];
-            if (skaicius < 0)
-                tekstas = "minus " + tekstas;
+                string tekstas = Pavadinimai[indeksas];
+                if (skaicius < 0)
+                    tekstas = "minus " + tekstas;
 
-            return tekstas;
+                return tekstas;
+            }
+
         }
 
+}    */
 
-
-
-
-    }
-}
